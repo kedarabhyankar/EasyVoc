f5f6a182bac9396ead5204bb7d1e2f55b0fa8fe7
Kedar Abhyankar <krabhyankar@gmail.com>
2020-07-11 18:45:23 -0700

    Added slicing of assignment name into categories to build folders and script names

--- EasyVoc.sh
+++ EasyVoc.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
 echo "What is the name of your Assignment?"
 echo "This should be in the assignment format, for example FA20-HW08-SomethingInJava."
 read assignmentName
 numDashes=$(echo $assignmentName| grep -o "-" | wc -l)
 numDashes=$(echo $numDashes| sed 's/ *$//g')
+
+assignmentSemester=$(echo $assignmentName | cut -d'-' -f 1)
+assignmentEnumeration=$(echo $assignmentName | cut -d'-' -f 2)
+assignmentName=$(echo $assignmentName | cut -d'-' -f 3)
+
+assignmentType=$(echo $assignmentEnumeration | head -c 2)
+assignmentNumber=$(echo $assignmentEnumeration | tail -c 3)
+echo "The assignment type is a $assignmentType, with number $assignmentNumber, has a a name of $assignmentName, and is part of $assignmentSemester."
