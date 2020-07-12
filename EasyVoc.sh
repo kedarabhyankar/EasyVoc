8e1b527947e21169f10c17c1c51039c792973f97
Kedar Abhyankar <krabhyankar@gmail.com>
2020-07-11 19:13:44 -0700

    Added directory creation and file name reading

--- EasyVoc.sh
+++ EasyVoc.sh
@@ -12,3 +12,15 @@ assignmentName=$(echo $assignmentName | cut -d'-' -f 3)
 
 assignmentType=$(echo $assignmentEnumeration | head -c 2)
 assignmentNumber=$(echo $assignmentEnumeration | tail -c 3)
+
+if [ "$assignmentType" != "HW" ] && [ "$assignmentType" != "LB" ] && [ "$assignmentType" != "PJ" ]; then
+	echo "The Assignment Type is invalid. It should be HW, PJ, or LB!"
+	exit 1
+fi
+
+mkdir -p $assignmentSemester/$assignmentEnumeration/
+
+echo "Type the names of the files the student should submit, including the extension, as space separated elements."
+echo "For example, you would type: fileOne.java fileTwo.java fileThree.java"
+read -a filenames
+
